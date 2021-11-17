658fb65c07a927a2240614b6ecd31fc37eb82528
AugustoBernardes <augustobdev@outlook.com>
2021-11-17 12:02:57 -0300

    Starting the project

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path =  require('path')
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+// ==================================================
+
+// Dotenv variables
+const PORT = process.env.PORT || 6565
+const DB_KEY = process.env.DB_KEY
+
+
+// Seting the tools 
+const app = express()
+app.use(cookieParser())
+app.use(express.urlencoded({extended:true}))
+
+// Db connection
+mongoose.connect(DB_KEY)
+
+const db = mongoose.connection
+
+db.once('error', () => {console.log(`DataBase wasn't load!`)})
+db.once('open', () => {console.log(`DataBase loaded!`)})
+
+// Setting the EJS
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'Pages'))
+
+// Routes
+app.use('/', (req,res) => {res.send('Working!!')})
+
+
+app.listen(PORT, () => {console.log(`Server running on PORT:${PORT}`)})
