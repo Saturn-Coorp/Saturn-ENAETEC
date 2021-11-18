a541dd773b7bcbebbef133a7fb03f110c996a27a
helayas <helayaswilson@gmail.com>
2021-11-18 13:10:06 -0300

    Adding CPF Validation

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -32,6 +32,39 @@ const AddNewComplaint = async (req,res) => {
         let receivedStreet = req.body.street.trim()
         let receivedImage = req.file.path
 
+        let k = String(receivedCpf)
+
+        console.log(k.length )  
+        if(!receivedCpf || k.length != 11 || k == "11111111111" || k == "22222222222" || k == "33333333333" || k == "44444444444" || k == "55555555555" || k == "66666666666" || k == "77777777777" || k == "88888888888" || k == "99999999999"){
+            res.status(400)
+            res.render('ErrorPage', {message:`2!`, url:'/'})
+        }
+
+        function verificationCPF(strCPF) {
+            let Sum;
+            let Rest;
+            Sum = 0;
+          if (strCPF == "00000000000") return false;
+        
+          for (i=1; i<=9; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+          Rest = (Sum * 10) % 11;
+        
+            if ((Rest == 10) || (Rest == 11))  Rest = 0;
+            if (Rest != parseInt(strCPF.substring(9, 10)) ) return false;
+        
+          Sum = 0;
+            for (i = 1; i <= 10; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+            Rest = (Sum * 10) % 11;
+        
+            if ((Rest == 10) || (Rest == 11))  Rest = 0;
+            if (Rest != parseInt(strCPF.substring(10, 11) ) ) return false;
+            return true;
+        }
+        
+        let verify = verificationCPF(receivedCpf)
+        console.log(verify)
+
+        
 
         try {
             const result =  await cloudinary.uploader.upload(receivedImage)
@@ -54,7 +87,7 @@ const AddNewComplaint = async (req,res) => {
             res.render('SuccessPage',{message:`Relato enviado !`, url:'/'})
         } catch (error) {
             res.status(400)
-            res.render('ErrorPage', {message:`Erro ao salvar os dados!`, url:'/'})
+            res.render('ErrorPage', {message:`4!`, url:'/'})
         }
     }
 
