17c8b252b320e8dac30a3eb2ea1e4719e272ec67
AugustoBernardes <augustobdev@outlook.com>
2021-11-18 22:20:57 -0300

    Creating the CPF validation to verify if it's a real CPF

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,7 +1,7 @@
 const cloudinary = require('../Utils/Cloudinary')
 const Complaint = require('../Models//ComplaintModel')
 const AllDistricts = require('../Controllers/Districts')
-const {complaintValidate} = require('../Controllers/ValidatingReceivedData')
+const {complaintValidate,verificationCPF} = require('../Controllers/ValidatingReceivedData')
 require('dotenv').config()
 // =====================================
 
@@ -22,7 +22,7 @@ const AddNewComplaint = async (req,res) => {
 
     if(error){
         res.status(400)
-        res.render('ErrorPage', {message:error, url:'/'})
+        res.render('ErrorPage', {message:`Complete todos os campos corretamente!`, url:'/'})
         // `Complete todos os campos!`
     }else{
         let receivedName = req.body.name.trim()
@@ -32,66 +32,37 @@ const AddNewComplaint = async (req,res) => {
         let receivedStreet = req.body.street.trim()
         let receivedImage = req.file.path
 
-        let k = String(receivedCpf)
+        let validatingCPF = verificationCPF(receivedCpf)
 
-        console.log(k.length )  
-        if(!receivedCpf || k.length != 11 || k == "11111111111" || k == "22222222222" || k == "33333333333" || k == "44444444444" || k == "55555555555" || k == "66666666666" || k == "77777777777" || k == "88888888888" || k == "99999999999"){
-            res.status(400)
-            res.render('ErrorPage', {message:`Erro!`, url:'/'})
-        }
-
-        function verificationCPF(strCPF) {
-            let Sum;
-            let Rest;
-            Sum = 0;
-          if (strCPF == "00000000000") return false;
+        if(validatingCPF === true){
+            try {
+                const result =  await cloudinary.uploader.upload(receivedImage)
         
-          for (i=1; i<=9; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-          Rest = (Sum * 10) % 11;
+                // Creating a new complaint
+                let complaint = {
+                    name:receivedName,
+                    cpf:receivedCpf,
+                    district:receivedDistrict,
+                    street:receivedStreet,
+                    content:receivedContent,
+                    image_url:result.secure_url,
+                    cloudinary_id:result.public_id
+                }
         
-            if ((Rest == 10) || (Rest == 11))  Rest = 0;
-            if (Rest != parseInt(strCPF.substring(9, 10)) ) return false;
-        
-          Sum = 0;
-            for (i = 1; i <= 10; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-            Rest = (Sum * 10) % 11;
-        
-            if ((Rest == 10) || (Rest == 11))  Rest = 0;
-            if (Rest != parseInt(strCPF.substring(10, 11) ) ) return false;
-            return true;
-        }
-        
-        let verify = verificationCPF(receivedCpf)
-        console.log(verify)
-
-        
-
-        try {
-            const result =  await cloudinary.uploader.upload(receivedImage)
-    
-            // Creating a new complaint
-            let complaint = {
-                name:receivedName,
-                cpf:receivedCpf,
-                district:receivedDistrict,
-                street:receivedStreet,
-                content:receivedContent,
-                image_url:result.secure_url,
-                cloudinary_id:result.public_id
+                complaint = new Complaint(complaint)
+                await complaint.save()
+                    
+                res.status(200)
+                res.render('SuccessPage',{message:`Relato enviado !`, url:'/'})
+            } catch (error) {
+                res.status(400)
+                res.render('ErrorPage', {message:`Erro ao enviar relato !`, url:'/'})
             }
-    
-            complaint = new Complaint(complaint)
-            await complaint.save()
-                
-            res.status(200)
-            res.render('SuccessPage',{message:`Relato enviado !`, url:'/'})
-        } catch (error) {
+        } else{
             res.status(400)
-            res.render('ErrorPage', {message:`4!`, url:'/'})
-        }
+            res.render('ErrorPage', {message:`Coloque um CPF valido!`, url:'/relatar'})
+        } 
     }
-
-    
 }
 
 module.exports = {LoadHomePage,LoadComplaintArea,AddNewComplaint}
--- Controllers/ValidatingReceivedData.js
+++ Controllers/ValidatingReceivedData.js
@@ -1,9 +1,10 @@
 const Joi = require('@hapi/joi')
 
+// Validating the received data
 const complaintValidate = (data) => {
     const schema = Joi.object({
         name:Joi.string().required(),
-        cpf:Joi.number().required(),
+        cpf:Joi.number().min(11).required(),
         district:Joi.string().required(),
         street:Joi.string().required(),
         content:Joi.string().required()
@@ -12,4 +13,29 @@ const complaintValidate = (data) => {
     return schema.validate(data)
 } 
 
-module.exports = {complaintValidate}
+// Validating  if the CPF is exist
+function verificationCPF(strCPF) {
+  let Sum;
+  let Rest;
+  Sum = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (i=1; i<=9; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+  Rest = (Sum * 10) % 11;
+
+  if ((Rest == 10) || (Rest == 11))  Rest = 0;
+  if (Rest != parseInt(strCPF.substring(9, 10)) ) return false;
+
+  Sum = 0;
+  for (i = 1; i <= 10; i++) Sum = Sum + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+  Rest = (Sum * 10) % 11;
+
+  if ((Rest == 10) || (Rest == 11))  Rest = 0;
+  if (Rest != parseInt(strCPF.substring(10, 11) ) ) return false;
+  return true;
+}
+
+
+
+
+module.exports = {complaintValidate,verificationCPF}
