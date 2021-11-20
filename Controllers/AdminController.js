d048edbc38d85a26ba2519c6b9e8fa201978c177
AugustoBernardes <augustobdev@outlook.com>
2021-11-19 21:35:23 -0300

    Finishing the admin area , so now it's over project ready!

--- Controllers/AdminController.js
+++ Controllers/AdminController.js
@@ -1,4 +1,5 @@
 const AdminUser = require('../Models/adminModel')
+const cloudinary = require('../Utils/Cloudinary')
 const Complaint = require('../Models//ComplaintModel')
 const jwt = require('jsonwebtoken')
 // =====================================
@@ -8,7 +9,12 @@ JWT_SECRET = process.env.JWT_SECRET
 
 const LoadLoginPage = (req, res) => {
     res.status(200)
-    res.render('AdminLoginPage')
+    res.redirect('/admin/menu')
+}
+
+const LoadAdminMenu = (req,res) => {
+    res.status(200)
+    res.render('AdminHome')
 }
 
 const verifyAdminPass = async (req,res) => {
@@ -23,17 +29,17 @@ const verifyAdminPass = async (req,res) => {
             let token = jwt.sign ({userPassword:password}, JWT_SECRET)
 
             // Sending the cookie
-            res.cookie('token', token, {maxAge:900000, httpOnly: true})
+            res.cookie('token', token, {maxAge:3600000, httpOnly: true})
 
             res.status(200)
             res.render('AdminHome')
         } else {
             res.status(400)
-            res.render('ErrorPage', {message:`Senha incorreta!`, url:'/admin/login'})
+            res.render('ErrorPage', {message:`Senha incorreta!`, url:'/admin/menu'})
         }
     } catch(error) {
         res.status(400)
-        res.render('ErrorPage', {message:`Senha incorreta!`, url:'/admin/login'})
+        res.render('ErrorPage', {message:`Senha incorreta!`, url:'/admin/menu'})
     }
 }
 
@@ -102,8 +108,100 @@ const ShowingTheComplaints = async (req,res) => {
     } catch (error) {
         
         res.status(400)
-        res.render('ErrorPage', {message:`Erro ao mostrar as denúncias!`, url:'/admin/login'})
+        res.render('ErrorPage', {message:`Erro ao mostrar as denúncias!`, url:'/admin/menu'})
     }
 }
 
-module.exports = {LoadLoginPage, verifyAdminPass, ShowingTheComplaints}
+const ViewTheComplaint = async (req,res) => {
+    let id = req.params.id
+
+    try {
+        let findComplaint = await Complaint.findById(id)
+
+        // Getting the status
+        let complaintStatus = findComplaint.status
+
+
+        if(complaintStatus === 'enviado'){
+            // Creating the buttons
+            var buttons = [
+                {
+                    message:'Enviar para analise',
+                    btnType:'btn-warning',
+                    url:`/admin/updatestatus/${id}/analizando`
+                },{
+                    message:'Negar denúncia',
+                    btnType:'btn-danger',
+                    url:`/admin/updatestatus/${id}/negado`
+                }
+            ]
+
+        }else if(complaintStatus === 'analizando'){
+            var buttons = [
+                {
+                    message:'Aprovar denúncia',
+                    btnType:'btn-success',
+                    url:`/admin/updatestatus/${id}/aprovado`
+                },{
+                    message:'Negar denuncia',
+                    btnType:'btn-danger',
+                    url:`/admin/updatestatus/${id}/negado`
+                }
+            ]
+        }else{
+            var buttons = [
+                {
+                    message:'Deletar',
+                    btnType:'btn-danger',
+                    url:`/admin/deletecomplaint/${id}`
+                }
+            ]
+        }
+
+        res.status(200)
+        res.render('ViewComplaint', {complaint:findComplaint, buttons:buttons,})
+    } catch (error) {
+        res.status(400)
+        res.render('ErrorPage', {message:`Não foi posivel carregar a denúncia!`, url:'/admin/menu'})
+    }
+}
+
+const updateTheStatus = async (req,res) => {
+    // Receiving the params
+    let id = req.params.id
+    let updatedStatus = req.params.status
+
+    try {
+        let complaint = await Complaint.findByIdAndUpdate(id, {status:updatedStatus})
+
+
+        res.status(200)
+        res.render('SuccessPage',{message:`Denúncia atualizada! `, url:'/admin/menu'})
+    }catch (error) {
+        res.status(400)
+        res.render('ErrorPage', {message:`Não foi posivel atualizar o status!`, url:'/admin/menu'})
+    }
+}
+
+const deleteComplaint = async (req,res) => {
+    let id = req.params.id
+
+    try {
+        // Getting the data
+        let complaint = await Complaint.findById(id)
+
+        // Deleting the Image
+        await cloudinary.uploader.destroy(complaint.cloudinary_id)
+
+        // Deleting the data
+        await complaint.remove()
+
+        res.status(200)
+        res.render('SuccessPage',{message:`Denúncia deletada! `, url:'/admin/menu'})
+    } catch (error) {
+        res.status(400)
+        res.render('ErrorPage', {message:`Não foi posivel deletar essa denúncia!`, url:'/admin/menu'})
+    }
+}
+
+module.exports = {LoadLoginPage, verifyAdminPass, ShowingTheComplaints,ViewTheComplaint,updateTheStatus,LoadAdminMenu,deleteComplaint}
--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -3,15 +3,22 @@ const methodOverride = require('method-override')
 const router = express.Router()
 // =====================================
 // Importing controllers
-const {LoadLoginPage, verifyAdminPass,ShowingTheComplaints} = require('../Controllers/AdminController')
+const {LoadLoginPage, verifyAdminPass,ShowingTheComplaints,ViewTheComplaint,updateTheStatus,LoadAdminMenu,deleteComplaint} = require('../Controllers/AdminController')
 const auth = require('../Controllers/Authentication')
 // =====================================
 router.use(methodOverride('_method'))
 
 // GET
 router.get('/login', LoadLoginPage)
+router.get('/menu',auth ,LoadAdminMenu)
 router.get('/complaints/:type',auth, ShowingTheComplaints)
+router.get('/view/:id',auth,ViewTheComplaint)
 
+// UPDATING STATUS
+router.get('/updatestatus/:id/:status',auth,updateTheStatus)
+
+// DELETING THE COMPLAINT
+router.get('/deletecomplaint/:id',auth,deleteComplaint)
 // POST
 router.post('/login', verifyAdminPass)
 
